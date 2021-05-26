0ff61cb7e19cddf422bbf26b8ee0ea92b39c4d6a
Joshua-Troy Meneses <jtmenese@gmail.com>
2021-05-25 19:37:55 -0700

    Reworked dash ability and added animation.

--- 189L Final/Assets/Scripts/PlayerAbilityDash.cs
+++ 189L Final/Assets/Scripts/PlayerAbilityDash.cs
@@ -10,44 +10,111 @@ namespace Player.Command
 {
     public class PlayerAbilityDash : MonoBehaviour, IPlayerCommand
     {
-        private float DashDistance = 0.5f;
+        [SerializeField]
+        private const float DASH_DISTANCE = 3f;
+        [SerializeField]
+        private const float ACTIVE_TIME = 0.2f;
+
+        // Total time dash has been active.
+        private float ElapsedTime;
+
+        // A float that has either a pos or negative 1 value, used to 
+        // teleport to the correct side during dash.
+        private float DirectionFacing;
         private GameObject Player;
         private Vector2 LastPosition;
+        private bool Active;
 
         void Start()
         {
-         
+            this.ElapsedTime = 0f;
+            this.Active = false;
+
         }
+
+        public void Update()
+        {
+            // Get animator attached to player.
+            var animator = this.Player.GetComponent<Animator>();
+            if (this.Active)
+            {
+                this.ElapsedTime += Time.deltaTime;
+                if (this.ElapsedTime < ACTIVE_TIME)
+                {
+                    // animator.SetBool("IsDashing", true);
+                    Debug.Log("TRUE");
+                    this.Player.transform.position = new Vector2(this.Player.transform.position.x + Time.deltaTime * DirectionFacing * DASH_DISTANCE, this.Player.transform.position.y);
+                }
+                else
+                {
+                    // animator.SetBool("IsDashing", false);
+                    this.Active = false;
+                }
+                // Update animator to set isDashing param to current state of ability.
+                animator.SetBool("IsDashing", this.Active);
+            }
+        }
+
 
         public void Execute(GameObject gameObject)
         {
-            this.Player = gameObject;
-            var rigidBody = gameObject.GetComponent<Rigidbody2D>();
-
-
-            if (rigidBody != null)
+            // Resets used if dash ability is currently not active.
+            if (!this.Active)
             {
-                // Check for facing in positive direction.
-                if (gameObject.GetComponent<SpriteRenderer>().flipX == false)
+                // Using command pattern, so need to set Player obj to
+                // param of Execute. 
+                this.Player = gameObject;
+                // Sets dash to active now.
+                this.Active = true;
+                this.ElapsedTime = 0f;
 
-                    // 1st approach
-                    // Boost the speed of the player
-                    // this.Player.GetComponent<Rigidbody2D>().velocity = new Vector2(rigidBody.velocity.x * DashDistance, rigidBody.velocity.y);
-
-                    // 2nd approach
-                    // Teleports the player to dodge projectiles
-                    // Combined with animation, it will give off dash effect
-                    this.Player.transform.position = new Vector3(this.Player.transform.position.x + DashDistance, this.Player.transform.position.y, 0.0f);
-
-                // Otherwise it will be moving in negative direction.
-                else
+                // Set direction of animation to correct direction.
+                this.DirectionFacing = 1f;
+                if (gameObject.GetComponent<SpriteRenderer>().flipX == true)
                 {
-                    //this.Player.GetComponent<Rigidbody2D>().velocity = new Vector2(rigidBody.velocity.x * DashDistance, rigidBody.velocity.y);
-                    this.Player.transform.position = new Vector3(this.Player.transform.position.x - DashDistance, this.Player.transform.position.y, 0.0f);
-                } 
-                Player.GetComponent<Animator>().SetTrigger("IsDashing");
+                    this.DirectionFacing = -1f;
+                }
+
             }
+
+
         }
+
+
+
+
+        // public void Execute(GameObject gameObject)
+        // {
+
+
+
+        //     this.Player = gameObject;
+        //     var rigidBody = gameObject.GetComponent<Rigidbody2D>();
+
+
+        //     if (rigidBody != null)
+        //     {
+        //         // Check for facing in positive direction.
+        //         if (gameObject.GetComponent<SpriteRenderer>().flipX == false)
+
+        //             // 1st approach
+        //             // Boost the speed of the player
+        //             // this.Player.GetComponent<Rigidbody2D>().velocity = new Vector2(rigidBody.velocity.x * DASH_DISTANCE, rigidBody.velocity.y);
+
+        //             // 2nd approach
+        //             // Teleports the player to dodge projectiles
+        //             // Combined with animation, it will give off dash effect
+        //             this.Player.transform.position = new Vector3(this.Player.transform.position.x + DASH_DISTANCE, this.Player.transform.position.y, 0.0f);
+
+        //         // Otherwise it will be moving in negative direction.
+        //         else
+        //         {
+        //             //this.Player.GetComponent<Rigidbody2D>().velocity = new Vector2(rigidBody.velocity.x * DASH_DISTANCE, rigidBody.velocity.y);
+        //             this.Player.transform.position = new Vector3(this.Player.transform.position.x - DASH_DISTANCE, this.Player.transform.position.y, 0.0f);
+        //         } 
+        //         Player.GetComponent<Animator>().SetTrigger("IsDashing");
+        //     }
+        // }
 
         //void Update()
         //{
