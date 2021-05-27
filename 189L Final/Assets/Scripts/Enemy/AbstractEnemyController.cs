b3a4447f343a5e3c2300523abc6158ccb864e9de
Tommy Saechao <tfsaechao@ucdavis.edu>
2021-05-27 11:19:30 -0700

    Created factory methods for enemy movement. Could be abstracted for global use in the game. TODO: Create behavior logic for enemy controller

--- 189L Final/Assets/Scripts/Enemy/AbstractEnemyController.cs
+++ 189L Final/Assets/Scripts/Enemy/AbstractEnemyController.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Enemy.Command;
+using Enemy.Movement;
+using Enemy.Constants;
+using Enemy.Behavior;
 
-public class AbstractEnemyController : MonoBehaviour
+public class AbstractEnemyController : MonoBehaviour, EnemyBehavior
 {
+    private MovementFactory Move;
+
+    void EnemyBehavior.Attack(GameObject gameObject)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void EnemyBehavior.Death(GameObject gameObject)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void EnemyBehavior.Idle(GameObject gameObject)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void EnemyBehavior.Roam(GameObject gameObject)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void EnemyBehavior.TakeDamage(GameObject gameObject)
+    {
+        throw new System.NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Move = new MovementFactory(EnemyConstants.BasicEnemySpeed, this.gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+
+
+
+
 }
