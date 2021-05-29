b2394784b2901a1ac82bf8c8f235995c45d1eb8f
Joshua-Troy Meneses <jtmenese@gmail.com>
2021-05-28 17:06:49 -0700

    Updated attack animations to be less choppy.
    Started work on BossController

---
+++ 189L Final/Assets/Scripts/Enemy/DemonKing/BossController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class BossController : MonoBehaviour
+    {
+
+        private enum State{
+        }
+
+        [SerializeField]
+        private float TotalHealth = 200f;
+
+        private float CurrentHealth;
+
+        private bool ActiveDamage; 
+
+
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
