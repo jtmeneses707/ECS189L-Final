209e712b6f0a033f491a39ea3be9716899cf032b
Tommy Saechao <tfsaechao@ucdavis.edu>
2021-06-10 07:13:40 -0700

    Fixed skeleton death

--- 189L Final/Assets/Scripts/Enemy/Skeleton/Skeleton_Death.cs
+++ 189L Final/Assets/Scripts/Enemy/Skeleton/Skeleton_Death.cs
@@ -7,8 +7,7 @@ public class Skeleton_Death : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        //Allows for animator to play full animation
-        animator.SetBool("IsDead", false);
+
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
